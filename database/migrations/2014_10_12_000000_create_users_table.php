8d8b5268652a25c567519b014818130261de152d
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-06-28 12:00:38 +0530

    init

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('role_id');
+
             $table->string('name')->nullable();
             $table->string('mobile')->nullable()->unique();
             $table->string('imei')->nullable();
