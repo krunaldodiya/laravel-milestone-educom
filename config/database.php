0d438a837ad903624ed2d25bab772297b3bbea86
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2019-07-07 09:56:30 +0530

    added date to timer

--- config/database.php
+++ config/database.php
@@ -44,6 +44,13 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'dump' => [
+                'dump_binary_path' => '/', // only the path, so without `mysqldump` or `pg_dump`
+                'use_single_transaction',
+                'timeout' => 60 * 5, // 5 minute timeout
+                'exclude_tables' => ['table1', 'table2'],
+                'add_extra_option' => '--optionname=optionvalue',
+            ],
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
@@ -119,7 +126,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' => Str::slug(env('APP_NAME', ' l aravel'), '_') . '_database_',
         ],
 
         'default' => [
