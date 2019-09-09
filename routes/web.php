554161caf521f0ddcaad7931c0b2974b49cf8820
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-09 17:17:41 +0530

    email testing

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,27 @@
 <?php
 
+use App\Mail\FeedbackMail;
+use App\User;
+use Illuminate\Support\Facades\Mail;
+
 Auth::routes();
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test', function () {
+    $user_id = 3;
+    $user = User::find($user_id);
+
+    $subject = "Feedback from {$user->name}";
+    $feedback = "test feedback";
+
+    Mail::to(env('MAIL_USERNAME'))->send(new FeedbackMail($user, $subject, $feedback));
+    return response(['message' => 'Feedback sent successfully.'], 200);
+});
+
 
 Route::group(['prefix' => 'export', 'middleware' => 'auth'], function () {
     Route::get("/users/{type}", "HomeController@exportUsers")->name("export.users");
