55868ab5e8c7a986337ec71c51bc04f2ee2694eb
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-17 06:46:26 +0530

    added telescope

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,10 @@
 <?php
 
-use App\Mail\FeedbackMail;
-use App\User;
-use Illuminate\Support\Facades\Mail;
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
 
 Auth::routes();
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/test', function () {
-    $user_id = 3;
-    $user = User::find($user_id);
-
-    $subject = "Feedback from {$user->name}";
-    $feedback = "test feedback";
-
-    Mail::to(env('MAIL_USERNAME'))->send(new FeedbackMail($user, $subject, $feedback));
-    return response(['message' => 'Feedback sent successfully.'], 200);
-});
-
 
 Route::group(['prefix' => 'export', 'middleware' => 'auth'], function () {
     Route::get("/users/{type}", "HomeController@exportUsers")->name("export.users");
@@ -44,7 +27,3 @@ Route::post('/feedback', 'HomeController@sendFeedback')->name('feedback');
 Route::get("/reset-device", "HomeController@resetDevice")->name("reset.device");
 
 Route::get("/storage/{url}", "HomeController@getAssets")->name("get-assets-from-storage")->where('url', '.*$');
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
