3ecf8591314ddea4ad4d5a120bcef8d2c9c6e106
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-19 12:34:20 +0530

    init

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -3,8 +3,13 @@
 @section('content')
 <div class="row">
     <div style="padding: 10px;">
-        <video width="320" height="240" controls controlsList="nodownload">
+        {{-- <video controls controlsList="nodownload">
             <source src="https://api.shendre.com/uploadfile/10th_Guj/Basic/Plus-minus-rules.mp4" type="video/mp4">
+        </video> --}}
+
+        <video width="320" height="240" controls>
+            <source src="https://api.shendre.com/uploadfile/10th_Guj/Basic/Plus-minus-rules.mp4" type="video/mp4">
+            Your browser does not support the video tag.
         </video>
     </div>
 </div>
