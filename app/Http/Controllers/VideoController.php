133f4d4fea8f8c9158aff56a5d2e9221e4e2f415
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-19 12:30:02 +0530

    init

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -16,6 +16,6 @@ class VideoController extends Controller
 
     public function play(Request $request)
     {
-        return 'hello';
+        view('play');
     }
 }
---
+++ resources/views/play.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<div class="row">
+    <div style="padding: 10px;">
+        <video width="320" height="240" controls controlsList="nodownload">
+            <source src="https://api.shendre.com/uploadfile/10th_Guj/Basic/Plus-minus-rules.mp4" type="video/mp4">
+        </video>
+    </div>
+</div>
+@endsection
