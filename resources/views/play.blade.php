7b948ad85dd791a5041b9d712de0d0d5c7028f18
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-20 13:24:43 +0530

    added player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,12 +1,4 @@
-<div class="fullscreen-bg">
-    <video controls autoplay disablePictureInPicture controlsList="nodownload" class="fullscreen-bg__video">
-        <source src="{{ $url }}" type="video/mp4">
-    </video>
-</div>
-
-
-<style>
-    .fullscreen-bg {}
-
-    .fullscreen-bg__video {}
-</style>
+<!-- The video -->
+<video autoplay muted loop id="myVideo" style="width: 100%; height: 100%">
+    <source src="{{ $url }}" type="video/mp4">
+</video>
---
+++ resources/views/play.copy.blade.php
@@ -0,0 +1,12 @@
+<div class="fullscreen-bg">
+    <video controls autoplay disablePictureInPicture controlsList="nodownload" class="fullscreen-bg__video">
+        <source src="{{ $url }}" type="video/mp4">
+    </video>
+</div>
+
+
+<style>
+    .fullscreen-bg {}
+
+    .fullscreen-bg__video {}
+</style>
