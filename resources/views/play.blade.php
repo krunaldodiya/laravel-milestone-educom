5a9ca4ff2ff9ad017fc679770886442925e021c4
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-20 13:33:13 +0530

    added player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,10 +1,24 @@
-<video controls autoplay disablePictureInPicture controlsList="nodownload" class="fullscreen-bg">
-    <source src="{{ $url }}" type="video/mp4">
-</video>
+<!DOCTYPE html>
+<html lang="en">
 
-<style>
-    .fullscreen-bg {
-        width: 100%;
-        height: 100%
-    }
-</style>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <title>Milestone Player</title>
+
+    <style>
+        .fullscreen-bg {
+            width: 100%;
+            height: 100%
+        }
+    </style>
+</head>
+
+<body>
+    <video controls autoplay disablePictureInPicture controlsList="nodownload" class="fullscreen-bg">
+        <source src="{{ $url }}" type="video/mp4">
+    </video>
+</body>
+
+</html>
