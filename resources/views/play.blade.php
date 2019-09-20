9ecd1f33f534b8eccc9ac0d289512e4a50e0fd36
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-20 14:13:59 +0530

    added player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> --}}
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1"> --}}
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
 
     <title>Milestone Player</title>
 </head>
