bef9d3f5123018cda662357829260fa85e7c24bf
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-20 13:55:21 +0530

    added player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -3,13 +3,29 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"> --}}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Milestone Player</title>
 </head>
 
+<style>
+    video#backgroundvid {
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        min-width: 100%;
+        min-height: 100%;
+        width: auto;
+        height: auto;
+        z-index: -100;
+        background: url(polina.jpg) no-repeat;
+        background-size: cover;
+    }
+</style>
+
 <body>
-    <video autoplay controls disablepictureinpicture controlslist="nodownload">
+    <video autoplay controls disablepictureinpicture controlslist="nodownload" id="backgroundvid">
         <source src="{{ $url }}" type="video/mp4">
     </video>
 </body>
