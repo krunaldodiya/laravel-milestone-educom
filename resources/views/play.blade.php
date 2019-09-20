54eb246d33a7ae62e10f32887ca5a4ff5a0e2d22
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-09-20 11:14:07 +0530

    added player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,11 +1,3 @@
-@extends('layouts.master')
-
-@section('content')
-<div class="row">
-    <div style="padding: 10px;">
-        <video controls controlsList="nodownload">
-            <source src="https://api.shendre.com/uploadfile/10th_Guj/Basic/Plus-minus-rules.mp4" type="video/mp4">
-        </video>
-    </div>
-</div>
-@endsection
+<video controls controlsList="nodownload">
+    <source src="https://api.shendre.com/uploadfile/10th_Guj/Basic/Plus-minus-rules.mp4" type="video/mp4">
+</video>
