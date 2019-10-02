229f8417394c80542150daf6ec41f5515c055f21
Krunal Dodiya <kunal.dodiya1@gmail.com>
2019-10-02 10:42:38 +0530

    added new version

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -9,42 +9,21 @@
 </head>
 
 <style>
-    body {
-        margin: 0px;
-    }
-
-    video::-internal-media-controls-download-button {
-        display: none;
-    }
-
-    video::-webkit-media-controls-enclosure {
-        overflow: hidden;
-    }
-
-    video::-webkit-media-controls-panel {
-        width: calc(100%);
-    }
-
     video#bgvideo {
-        position: fixed;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        min-width: 100%;
-        min-height: 100%;
-        width: 100%;
-        height: 100%;
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        bottom: 0px;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        margin: auto;
     }
 </style>
 
 <body style="background-color: black">
-    {{-- <video disablepictureinpicture controls controlslist="nodownload" id="bgvideo">
+    <video disablepictureinpicture controls autoplay controlslist="nodownload" name="media" id="bgvideo">
         <source src="{{ $url }}" type="video/mp4">
-    </video> --}}
-
-    <video controls="" autoplay="" name="media">
-        <source src="https://api.shendre.com/uploadfile/10th_Guj/intro/intro-compressed.mp4" type="video/mp4">
     </video>
 </body>
 
