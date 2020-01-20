572d4f4d1b0c10854258c2702b368126f0a704ae
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-20 10:10:11 +0530

    added institute login

--- database/migrations/2019_06_22_033913_create_subscriptions_table.php
+++ database/migrations/2019_06_22_033913_create_subscriptions_table.php
@@ -15,9 +15,19 @@ class CreateSubscriptionsTable extends Migration
     {
         Schema::create('subscriptions', function (Blueprint $table) {
             $table->bigIncrements('id');
+
             $table->bigInteger("institute_id")->unsigned()->default(1);
+            $table->foreign('institute_id')->references('id')->on('institutes')
+                ->onUpdate('cascade')->onDelete('cascade');
+
             $table->bigInteger("category_id")->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onUpdate('cascade')->onDelete('cascade');
+
             $table->bigInteger("user_id")->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
             $table->timestamp('expires_at');
             $table->timestamps();
         });
