8c07e4a0d0703ff7b84e9307fcd4b91aa084adf4
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-23 17:38:24 +0530

    subscription fix

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Institute;
 use Illuminate\Http\Request;
 use App\Subscription;
 use Carbon\Carbon;
@@ -20,13 +21,13 @@ class SubscriptionController extends Controller
     {
         $user = auth('api')->user();
         $category_id = $request->category_id;
-        $institute_id = $request->institute_id ? $request->institute_id : 1;
+        $expires_at = $request->institute_id ? Institute::find($request->institute_id) : Carbon::now()->addDays($user->site_settings->expiry);
 
         Subscription::create([
-            'institute_id' => $institute_id,
+            'institute_id' => $request->institute_id,
             'category_id' => $category_id,
             'user_id' => $user->id,
-            'expires_at' => Carbon::now()->addDays(2)
+            'expires_at' => $expires_at
         ]);
 
         return response(['user' => $this->userRepo->getUserById($user->id)], 200);
