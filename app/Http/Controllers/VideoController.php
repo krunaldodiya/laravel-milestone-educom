8fff7ddb70504d1cc952d9018adf655e61143b00
Krunal Dodiya <krunaldodiya@Krunals-iMac.local>
2020-01-24 10:03:48 +0530

    subscription fix

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -16,16 +16,10 @@ class VideoController extends Controller
 
     public function play(Request $request)
     {
-        $headers = $request->get('headers');
+        // $headers = $request->get('headers');
 
         $url = $request->get('url');
 
-        // return view('play')->with('url', $url);
-
-        if ($headers && $headers['download'] == 'none') {
-            return view('play')->with('url', $url);
-        }
-
-        return view('play')->with('url', "https://player.vimeo.com/video/346374153?title=0&portrait=0&byline=0&autoplay=1");
+        return view('play')->with('url', $url);
     }
 }
